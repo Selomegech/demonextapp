e4fbc481a82b67cfb16881d1950ac89d2e49d390
selome <selomegech@gmail.com>
2024-08-09 16:11:18 +0300

    chnge

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      you are looged in 
-      <button onClick={() => signOut()} className='px-4 py-2 bg-slate-900'>Sign out</button>
-    </div>
+    <><div>
+    you are looged in 
+    <button onClick={() => signOut()} className='px-4 py-2 bg-slate-900'>Sign out</button>
+  </div></>
   )
 }
 
